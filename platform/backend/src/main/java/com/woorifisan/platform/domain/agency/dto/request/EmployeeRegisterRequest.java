c350c7224274ded6e83eed17a6c0a85c350b49f0
unknown <77kangsan60@gmail.com>
2026-05-25 03:33:33 +0900

    refactor(platform): agency 패키지 내부 DTO 클래서 Recode -> getter/setter Annotation 형식으로 변경

--- platform/backend/src/main/java/com/woorifisan/platform/domain/agency/dto/request/EmployeeRegisterRequest.java
+++ platform/backend/src/main/java/com/woorifisan/platform/domain/agency/dto/request/EmployeeRegisterRequest.java
@@ -2,22 +2,28 @@ package com.woorifisan.platform.domain.agency.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
-public record EmployeeRegisterRequest(
-        @NotBlank(message = "ID는 필수입니다.")
-        String loginId,
-        
-        @NotBlank(message = "비밀번호는 필수입니다.")
-        String password,
-        
-        @NotBlank(message = "권한은 필수입니다.")
-        String role,
-        
-        @NotNull(message = "소속 대행업체 ID는 필수입니다.")
-        Long agencyId,
-        
-        @NotBlank(message = "대행업체 사번은 필수입니다.")
-        String employeeNum
-) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeRegisterRequest {
+    @NotBlank(message = "ID는 필수입니다.")
+    private String loginId;
+    
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+    
+    @NotBlank(message = "권한은 필수입니다.")
+    private String role;
+    
+    @NotNull(message = "소속 대행업체 ID는 필수입니다.")
+    private Long agencyId;
+    
+    @NotBlank(message = "대행업체 사번은 필수입니다.")
+    private String employeeNum;
 }
