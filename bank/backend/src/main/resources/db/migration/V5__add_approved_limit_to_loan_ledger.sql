cf20d6ad74ac157132f48ccf77efc56270d29c78
김동욱 <140793632+aransword@users.noreply.github.com>
2026-05-29 17:18:25 +0900

    Revert "Chore/workflows update (#125)" (#127)
    This reverts commit 3a99ba4d3a99460ec513f9bb34438ee1f6253697.

--- bank/backend/src/main/resources/db/migration/V5__add_approved_limit_to_loan_ledger.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE loan_ledger
-    ADD COLUMN approved_limit DECIMAL(18,2) COMMENT '심사 승인 한도 (실행 시 한도 초과 검증용)';
--- platform/frontend/src/routes/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuthStore } from '../store/useAuthStore';
-
-const PrivateRoute = () => {
-  const { accessToken } = useAuthStore();
-  return accessToken ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
