4b570262a777576dc94ce53f8be238b6dba93de1
unknown <77kangsan60@gmail.com>
2026-06-01 14:12:37 +0900

    fix(monitoring): LogRequest MaxPage Size 지정

--- monitoring/backend/src/main/java/com/woorifisan/monitoring/domain/log/dto/request/LogRequest.java
+++ monitoring/backend/src/main/java/com/woorifisan/monitoring/domain/log/dto/request/LogRequest.java
@@ -1,6 +1,7 @@
 package com.woorifisan.monitoring.domain.log.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ public class LogRequest extends LogSearchRequest {
 
     @Schema(description = "페이지 크기", example = "20")
     @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다.")
+    @Max(value = 20, message = "페이지 크기는 20 이하이어야 합니다.")
     private int size = 20;
 
     public int getOffset() {
